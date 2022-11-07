83ca8d6729b6af4f348e12791b6a917c7e4b1594
Charles Kerr <punt1959@users.noreply.github.com>
2022-11-07 17:54:10 -0500

    initial

---
+++ source/main.hpp
@@ -0,0 +1,12 @@
+//Copyright © 2022 UOX Development. All rights reserved.
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <cstdint>
+#include <string>
+
+//=================================================================================
+
+
+#endif /* main_hpp */
